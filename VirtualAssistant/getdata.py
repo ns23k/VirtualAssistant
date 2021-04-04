0af36c8b9040c9bcba2e3b71f894addf207b7724
Naman23-coder <78961713+Naman23-coder@users.noreply.github.com>
2021-04-04 19:51:04 +0530

    Update getdata.py

--- VirtualAssistant/getdata.py
+++ VirtualAssistant/getdata.py
@@ -1,2 +1,12 @@
 '''this file would contain code that would fetch data and return it to user'''
 # work in progress
+import datetime
+from .audioinout import speak
+
+def speaktime(format= 'thour'):
+    if format == 'thour':
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")
+        speak(f"Sir, the time is {strTime}")
+    elif  format == 'tfhour':
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")
+        speak(f"Sir, the time is {strTime}")
