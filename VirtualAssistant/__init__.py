ce3370655f7c7795c3cd3e93983fa8f8ee1ca211
Naman23-coder <78961713+Naman23-coder@users.noreply.github.com>
2021-04-05 22:14:26 +0530

    Update __init__.py

--- VirtualAssistant/__init__.py
+++ VirtualAssistant/__init__.py
@@ -1,4 +1,4 @@
 from .audioinout import takecommand,speak,getvoices
 from .getdata import speaktime
-from .internet import opengoogle,openfacebook,openwebsite,openwhatsappweb,openyoutube,opengithub,searchgoogle
+from .internet import opengoogle,openfacebook,openwebsite,openwhatsappweb,openyoutube,opengithub,searchgoogle,playvidonyt,speakwikisummary
 from .computer import opencalc,openfile,openmsword,openvscode
