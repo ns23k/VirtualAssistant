5f5f598f5588eeed484df0d0a34b79e42c950d1f
Naman23-coder <78961713+Naman23-coder@users.noreply.github.com>
2021-04-05 12:24:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name='VirtualAssistant',
     version='0.0.1',
     packages=['VirtualAssistant'],
+    install_requires=['pyttsx3','SpeechRecognition','colorama'],
     url='https://github.com/Naman23-coder/VirtualAssistant',
     license='MIT',
     author='Naman Sharma , Devansh Sareen',
     author_email='namansharma232009@gmail.com , devanshsareen@gmail.com',
-    description='VirtualAssistant is a python package that allowas user to make simple VirtualAssistants with already defined commands with only one import.'
+    description='VirtualAssistant is a python package that allowas user to make simple Virtual Assistants with already defined commands with only one import.'
 )
