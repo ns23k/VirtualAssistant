abd8b509662bc8f7a7d65db3c3a0005903ee7019
Naman23-coder <78961713+Naman23-coder@users.noreply.github.com>
2021-04-05 10:16:28 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='VirtualAssistant',
+    version='0.0.1',
+    packages=['VirtualAssistant'],
+    url='https://github.com/Naman23-coder/VirtualAssistant',
+    license='MIT',
+    author='Naman Sharma , Devansh Sareen',
+    author_email='namansharma232009@gmail.com , devanshsareen@gmail.com',
+    description='VirtualAssistant is a python package that allowas user to make simple VirtualAssistants with already defined commands with only one import.'
+)
