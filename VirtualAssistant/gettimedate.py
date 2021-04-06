111286dbf79f7a0f3a4251fcf9a7ab70175c16ad
Naman23-coder <78961713+Naman23-coder@users.noreply.github.com>
2021-04-06 12:11:17 +0530

    Update and rename getdata.py to gettimedate.py

--- VirtualAssistant/gettimedate.py
+++ VirtualAssistant/gettimedate.py
@@ -10,3 +10,18 @@ def speaktime(format= 'thour'):
     elif  format == 'tfhour':
         strTime = datetime.datetime.now().strftime("%H:%M:%S")
         speak(f"Sir, the time is {strTime}")
+
+        def wishme(assistname = None):
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+    if assistname is None:
+        speak("I am your VirtualAssistant. Please tell me how may I help you")
+    else:
+        speak(f"I am {assistname}. Please tell me how may I help you")
