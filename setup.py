f3ecde39c870126467ec9973b62cb492c79177e3
Naman sharma <78961713+Naman23-coder@users.noreply.github.com>
2021-04-24 22:13:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
 
+def readme():
+    with open(r'README.md') as f:
+        README = f.read()
+    return README
+# contributer = Devansh Sareen
 setup(
     name='VirtualAssistant',
-    version='0.0.1',
+    version='0.0.2.0',
     packages=['VirtualAssistant'],
     install_requires=['pyttsx3','SpeechRecognition','colorama','pywhatkit'],
     url='https://github.com/Naman23-coder/VirtualAssistant',
     license='MIT',
-    author='Naman Sharma , Devansh Sareen',
-    author_email='namansharma232009@gmail.com , devanshsareen@gmail.com',
+    author='Naman Sharma',
+    author_email = "namansharma232009@gmail.com",
+    include_package_data=True,
+    long_description=readme(),
+    long_description_content_type="text/markdown",
     description='VirtualAssistant is a python package that allows user to make simple Virtual Assistants with already defined commands with only one import.'
+    
 )
+
+
